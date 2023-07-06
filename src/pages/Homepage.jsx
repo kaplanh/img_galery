d741e3af4724b4b4e4b1855d55aebd94b2e8cd2a
kplnhsyn <kplnhsyn.49@gmail.com>
2023-07-06 15:58:12 +0200

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,35 @@
+import Pictures from "../compenents/Pictures";
+import data from "../helpers/data";
+import "./Homepage.css";
+
+const Homepage = () => {
+    // console.log(data);
+    return (
+        <div className="row gap-1 justify-content-center">
+            {data.map((item) => {
+                console.log(item);
+                const { photographer, src } = item;
+                return (
+                    <div class="col col-sm-6 col-md-3 col-lg-2">
+                        <div
+                            class=" card text-bg-light "
+                            
+                        >
+                            <div class=" card-body  border border-3 rounded  text-center d-flex flex-column ">
+                                <img
+                                    src={src.large}
+                                    class="card-img-top img-thumbnail  "
+                                    alt="..."
+                                    style={{ height: "268px" }}
+                                ></img>
+                                <h5 class="card-title">{photographer}</h5>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Homepage;
